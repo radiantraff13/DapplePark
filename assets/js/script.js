a02b6ebe318ae02fd0c8c5a89d51553b215fdccd
Razi Hossain Rafi <145110417+radiantraff13@users.noreply.github.com>
2025-07-01 23:42:40 +1000

    Upload assets folder 📁🎨
    Added all images, CSS, JS, and media files required for the DapplePark user interface and styling.

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	$('#carousel-one').carousel();
+    $('#carousel-two').carousel();
+    $('#carousel-three').carousel();
+    $('#carousel-four').carousel();
+	$('#carousel-five').carousel();
+	$('#carousel-six').carousel();
+    $('#carousel-seven').carousel();
+
+
+
+})
+
+$(document).ready(function(){
+	$('#carousel-one, #carousel-two, #carousel-three, #carousel-four, #carousel-five, #carousel-six, #carousel-seven').carousel();
+})
+
+$(document).ready(function(){
+	$('#carousel-one').carousel({
+		interval: 2000,
+		keyboard: false,
+		pause: hover
+	});
+})
+
+$(document).ready(function(){
+	$('#carousel-two').carousel({
+		interval: 2000,
+		keyboard: false,
+		pause: hover
+	});
+})
+$(document).ready(function(){
+	$('#carousel-three').carousel({
+		interval: 2000,
+		keyboard: false,
+		pause: hover
+	});
+})
+$(document).ready(function(){
+	$('#carousel-four').carousel({
+		interval: 2000,
+		keyboard: false,
+		pause: hover
+	});
+})
+$(document).ready(function(){
+	$('#carousel-five').carousel({
+		interval: 2000,
+		keyboard: false,
+		pause: hover
+	});
+})
+$(document).ready(function(){
+	$('#carousel-six').carousel({
+		interval: 2000,
+		keyboard: false,
+		pause: hover
+	});
+})
+$(document).ready(function(){
+	$('#carousel-seven').carousel({
+		interval: 2000,
+		keyboard: false,
+		pause: hover
+	});
+})
